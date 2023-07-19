eccf73837b752c628e84c5848d19936b70429e9a
Nicholas Manning <njmanning212@gmail.com>
2023-07-19 11:44:28 -0500

    create cat show controller and route

--- controllers/cats.js
+++ controllers/cats.js
@@ -20,7 +20,18 @@ const index = async (req, res) => {
   }
 }
 
+const show = async (req, res) => {
+  try {
+    const cat = await Cat.findByPk(req.params.id)
+    res.status(200).json(cat)
+  } catch (error) {
+    console.log(error)
+    req.status(500).json(error)
+  }
+}
+
 module.exports = {
   create,
   index,
+  show,
 }
--- routes/cats.js
+++ routes/cats.js
@@ -3,5 +3,6 @@ const catsCtrl = require('../controllers/cats.js')
 
 router.post('/', catsCtrl.create)
 router.get('/', catsCtrl.index)
+router.get('/:id', catsCtrl.show)
 
 module.exports = router
